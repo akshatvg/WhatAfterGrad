5f5269c99f4bc079e8a4de90dabbaa5b562780d7
Akshat Gupta <akshatvg23@gmail.com>
2020-06-15 03:51:52 +0530

    navbar

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+(function ($) {
+    "use strict";
+
+    // Loader
+    $(function () {
+        var loader = function () {
+            setTimeout(function () {
+                if ($('#loader').length > 0) {
+                    $('#loader').removeClass('show');
+                }
+            }, 1);
+        };
+        loader();
+    });
+
+    // Auto Init 
+    M.AutoInit();
+
+})(jQuery);
+
+function searchFocus() {
+    $("#search").focus();
+}
+
+console.clear();
