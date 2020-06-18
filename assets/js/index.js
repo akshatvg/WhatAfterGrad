9e011fbd229f413a35f2a39dd34ba31b577d4273
Sandeep <rayanuthalas@gmail.com>
2020-06-18 08:04:50 +0530

    Added login

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,4 +19,51 @@
 
 })(jQuery);
 
-console.clear();
+function Login() {
+    if (!Validateemail()) { } else {
+        if (!ValidatePass()) { } else {
+            document.getElementById("signin").disabled = true
+            let fetchData = {
+                method: 'POST',
+                body: JSON.stringify({
+                    "username": document.getElementById("email").value,
+                    "password": document.getElementById("password").value
+                }),
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+            fetch(LOGIN_URL, fetchData)
+                .then(function (res) {
+                    if (res.status == 401) {
+                        ShowToast("Wrong credentials!")
+                        document.getElementById("password").value = ''
+                        document.getElementById("signin").disabled = false
+                    } else if (res.status == 200) {
+                        res.json().then(function (data) {
+                            if (data == null) { } else {
+                                ShowToast("Login Success")
+                                localStorage.setItem("TOKEN", "Token " + data.user.token)
+                                localStorage.setItem("FULL_NAME", data.user.full_name)
+                                document.getElementById("signin").disabled = false
+                                return window.location.replace("/");
+                            }
+                        })
+                    } else {
+                        ShowToast("Server error. Our team has been informed. Working on it.");
+                        document.getElementById("email").value = '';
+                        document.getElementById("password").value = '';
+                        document.getElementById("signin").disabled = false
+                    }
+                    return;
+                })
+                .catch((error) => { })
+        }
+    }
+}
+
+
+
+
+// console.clear();
