b3320e68a921547bee5fb810d348de9ce46507f4
Akshat Gupta <akshatvg23@gmail.com>
2020-06-18 05:50:09 +0530

    submissions done

--- assets/js/submissionsTable.js
+++ assets/js/submissionsTable.js
@@ -169,5 +169,21 @@
 
 // DataTable Function
 $(document).ready(function () {
-    $('#viewSubmissions').dataTable();
+    $('#viewSubmissions').dataTable({
+        "oLanguage": {
+            "sStripClasses": "",
+            "sSearch": "",
+            "sSearchPlaceholder": "Enter Keywords Here",
+            "sInfo": "_START_ -_END_ of _TOTAL_",
+            "sLengthMenu": '<span>Rows per page:</span><select class="browser-default">' +
+                '<option value="10">10</option>' +
+                '<option value="20">20</option>' +
+                '<option value="30">30</option>' +
+                '<option value="40">40</option>' +
+                '<option value="50">50</option>' +
+                '<option value="-1">All</option>' +
+                '</select></div>'
+        },
+        bAutoWidth: false
+    });
 });
