f5ef953d753f6ad82344a25a379da9ae906a1072
Akshat Gupta <akshatvg23@gmail.com>
2020-06-22 02:54:03 +0530

    create influencer

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,6 +1,6 @@
 
 BASE_URL = "https://whataftergrad.herokuapp.com/";
-LOGIN_URL = BASE_URL + "api/auth/admin/sigin/";
+LOGIN_URL = BASE_URL + "api/auth/admin/sigin";
 QUESTION_ADD = BASE_URL + "api/admin/questions/add"
 
 
